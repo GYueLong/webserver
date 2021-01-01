1765f88e5653238d3ce55abdd45de3bef69a0c1c
gaoyuelong <gyldeyx@qq.com>
2021-01-01 21:11:30 +0800

    new file

---
+++ include/head.h
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: head.h
+	> Author: gyl
+	> Mail: gyldeyx@qq.com
+	> Created Time: Fri 01 Jan 2021 09:09:34 PM CST
+ ************************************************************************/
+
+#ifndef _HEAD_H
+#define _HEAD_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#endif
---
+++ main.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: main.c
+	> Author: gyl
+	> Mail: gyldeyx@qq.com
+	> Created Time: Fri 01 Jan 2021 09:10:23 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+    printf("hello world!\n");
+    return 0;
+}
